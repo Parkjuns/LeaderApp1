363347e7894daac28e85faaba50124f0468df7a3
eunji <salasoft2013@gmail.com>
2023-05-15 10:17:11 +0900

    Initial Commit

---
+++ LeaderApp/LeaderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LeaderAppApp.swift
+//  LeaderApp
+//
+//  Created by leaders on 2023/05/15.
+//
+
+import SwiftUI
+
+@main
+struct LeaderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
